c38e30cf8445f9a3e754dd6081a726167d45cbe2
Christopher Gutteridge <totl@soton.ac.uk>
2016-09-12 22:55:04 +0100

    bugfixes

--- database/migrations/2016_09_07_113857_create_report_types_table.php
+++ database/migrations/2016_09_07_113857_create_report_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReportTypeTable extends Migration
+class CreateReportTypesTable extends Migration
 {
     /**
      * Run the migrations.
---
+++ database/migrations/2016_09_10_191203_create_reports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('document_revision_id')->unsigned();
+            $table->index('document_revision_id');
+            $table->longText('data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reports');
+    }
+}
