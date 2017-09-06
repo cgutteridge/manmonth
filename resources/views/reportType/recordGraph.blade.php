83b26a2390bfc2f8553ff6054c5ea037738da516
Christopher Gutteridge <totl@soton.ac.uk>
2017-09-06 16:59:00 +0100

    no green red with no target

--- resources/views/reportType/recordGraph.blade.php
+++ resources/views/reportType/recordGraph.blade.php
@@ -1,31 +1,35 @@
 <div class="mm-graph">
     <div class="mm-target-bar">
-        @if( $total == $target && $target != 0)
-            <div class="mm-target mm-target-alloc" style="width: {{ 99*$total*$scale }}%">
-                <div class="mm-target-inner">{{ $total }} {{$units}} allocated</div>
-            </div>
-        @endif
-        @if( $target > $total )
-            @if( $total > 0 )
+        @if( $target == 0 )
+            &nbsp;
+        @else
+            @if( $total == $target )
                 <div class="mm-target mm-target-alloc" style="width: {{ 99*$total*$scale }}%">
                     <div class="mm-target-inner">{{ $total }} {{$units}} allocated</div>
                 </div>
             @endif
-            @if( $showFree )
-                <div class="mm-target mm-target-free" style="width: {{ 99*($target-$total)*$scale}}%">
-                    <div class="mm-target-inner">{{ $target-$total }} {{$units}} free</div>
+            @if( $target > $total )
+                @if( $total > 0 )
+                    <div class="mm-target mm-target-alloc" style="width: {{ 99*$total*$scale }}%">
+                        <div class="mm-target-inner">{{ $total }} {{$units}} allocated</div>
+                    </div>
+                @endif
+                @if( $showFree )
+                    <div class="mm-target mm-target-free" style="width: {{ 99*($target-$total)*$scale}}%">
+                        <div class="mm-target-inner">{{ $target-$total }} {{$units}} free</div>
+                    </div>
+                @endif
+            @endif
+            @if( $target < $total )
+                @if( $target > 0 )
+                    <div class="mm-target mm-target-alloc" style="width: {{ 99*$target*$scale }}%">
+                        <div class="mm-target-inner">{{ $target }} {{$units}} meets target</div>
+                    </div>
+                @endif
+                <div class="mm-target mm-target-over" style="width: {{ 99*($total-$target)*$scale }}%">
+                    <div class="mm-target-inner">{{ $total-$target }} {{$units}} overload</div>
                 </div>
             @endif
-        @endif
-        @if( $target < $total )
-            @if( $target > 0 )
-                <div class="mm-target mm-target-alloc" style="width: {{ 99*$target*$scale }}%">
-                    <div class="mm-target-inner">{{ $target }} {{$units}} meets target</div>
-                </div>
-            @endif
-            <div class="mm-target mm-target-over" style="width: {{ 99*($total-$target)*$scale }}%">
-                <div class="mm-target-inner">{{ $total-$target }} {{$units}} overload</div>
-            </div>
         @endif
     </div>
     <div class="mm-loading-bar">
